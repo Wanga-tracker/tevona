0b320e0d7489a6544f395661f55b1c750fdb0b3d
Wanga-tracker <trackerwanga01@gmail.com>
2025-08-19 04:21:39 +0300

    Update yt-search.d.ts

--- src/types/yt-search.d.ts
+++ src/types/yt-search.d.ts
@@ -1,1 +1,19 @@
-declare module "yt-search";
+declare module "yt-search" {
+  export interface YTAuthor {
+    name: string;
+  }
+
+  export interface YTVideo {
+    title: string;
+    videoId: string;
+    author: YTAuthor;
+    duration: string;
+    views: number;
+  }
+
+  export interface YTResult {
+    videos: YTVideo[];
+  }
+
+  export default function yts(query: string): Promise<YTResult>;
+}
